690dbe57f0f25adc5649458d2c07e7214a96d200
Xeexen <andres.o.mm@hotmail.com>
2025-05-14 19:16:31 -0500

    Add get mapping

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('Hola Digital Ocean');
+        return response()->json('Hola Digital Ocean', 200);
     }
 }
