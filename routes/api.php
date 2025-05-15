2c1a8153ffc32f85ce8c12fe6d2904d3371447a8
Xeexen <andres.o.mm@hotmail.com>
2025-05-14 19:11:42 -0500

    Add get

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,4 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('home', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('home', [App\Http\Controllers\HomeController::class, 'index']);
